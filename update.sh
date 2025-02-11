cc3a8e20671bd15ea8a739f8f7bcf524343e88e3
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-02-11 10:06:39 -0500

    Update to moonc v0.1.20250210+7be093d1f
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -ex
 moon update && moon install && rm -rf target
-moon fmt
-# All targets except for 'js' and 'wasm-gc' currently fail: https://github.com/moonbitlang/core/issues/1219
-# moon test --target native
-# moon test --target all
-moon test --target js
-moon test --target wasm-gc
+moon fmt && moon info
+moon test --target native
+moon test --target all
